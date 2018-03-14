ca94fd7f0460cab2e6e89c6436dec74de13b944e
insoman <info@dreampics.pl>
2018-03-14 17:26:57 +0100

    Add comment to home class

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,7 +3,11 @@
 class Home extends Controller
 {
 
-    
+    /**
+     * Constructor require current controller name as param
+     * 
+     * @param $controllerName   Current controllers name such as 'home' or 'posts'
+     */
     public function __construct($controllerName) {
         // echo 'We are at home';
         // echo $controllerName;
